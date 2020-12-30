e29e5b73da618dd1a0f11a63a821a59183078ca2
KirtiPandey512 <kirtipandey512@gmail.com>
2020-12-30 12:23:00 +0530

    Revert "Commit"
    This reverts commit f6e2b4aeceed2ba5883ae23cfd48bf0314c069d3.

--- Login Module/src/com/Login/DAO/LoginDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.Login.DAO;
-
-import java.sql.SQLException;
-
-import com.Login.entity.User;
-
-public interface LoginDAOInterface {
-	public boolean isValid(User user) throws SQLException;
-}
